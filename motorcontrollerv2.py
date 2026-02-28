30edab1f219d6b91aff155734d169572860426d1
Jbamidi <jassubamidi24@gmail.com>
2026-02-28 09:09:57 -0600

    motor b

--- motorcontrollerv2.py
+++ motorcontrollerv2.py
@@ -4,34 +4,34 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([5, 12, 16], GPIO.OUT)
 
-# Turn on standby
+print("All LOW - motor should stop")
+GPIO.output(16, GPIO.LOW)
+GPIO.output(5, GPIO.LOW)
+GPIO.output(12, GPIO.LOW)
+time.sleep(3)
+
+print("Turning STBY on...")
 GPIO.output(16, GPIO.HIGH)
 
-# Start PWM at 60% speed
 pwm = GPIO.PWM(12, 1000)
 pwm.start(60)
 
-# FORWARD: Pi LOW -> MOSFET OFF -> 10K pulls to 5V -> AIN1 HIGH
 print("FORWARD...")
 GPIO.output(5, GPIO.LOW)
 time.sleep(3)
 
-# STOP
 print("STOPPING...")
 pwm.ChangeDutyCycle(0)
 time.sleep(2)
 
-# BACKWARD: Pi HIGH -> MOSFET ON -> AIN1 pulled to GND -> AIN1 LOW
 print("BACKWARD...")
 pwm.ChangeDutyCycle(60)
 GPIO.output(5, GPIO.HIGH)
 time.sleep(3)
 
-# STOP
 print("STOPPING...")
 pwm.ChangeDutyCycle(0)
-time.sleep(1)
 
-print("DONE")
 pwm.stop()
 GPIO.cleanup()
+print("DONE")
