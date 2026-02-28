5a3d5afd17e344187fb70673705f9768a6f88ef5
Jbamidi <jassubamidi24@gmail.com>
2026-02-28 03:58:40 -0600

    motor a test

--- motorcontrollerv2.py
+++ motorcontrollerv2.py
@@ -1,29 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
-AIN1_CONTROL = 5
 PWMA = 12
 STBY = 16
+AIN1 = 5
 
 def test():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup([AIN1_CONTROL, PWMA, STBY], GPIO.OUT)
+    GPIO.setup([PWMA, STBY, AIN1], GPIO.OUT)
     GPIO.output(STBY, GPIO.HIGH)
     time.sleep(0.5)
 
     pwm_a = GPIO.PWM(PWMA, 1000)
     pwm_a.start(0)
 
-    print("GPIO HIGH - transistor ON - collector pulls LOW")
-    GPIO.output(AIN1_CONTROL, GPIO.HIGH)
+    print("Test 1: AIN1 = HIGH")
+    GPIO.output(AIN1, GPIO.HIGH)
     pwm_a.ChangeDutyCycle(100)
     time.sleep(3)
 
     pwm_a.ChangeDutyCycle(0)
     time.sleep(2)
 
-    print("GPIO LOW - transistor OFF - collector pulled to 5V")
-    GPIO.output(AIN1_CONTROL, GPIO.LOW)
+    print("Test 2: AIN1 = LOW")
+    GPIO.output(AIN1, GPIO.LOW)
     pwm_a.ChangeDutyCycle(100)
     time.sleep(3)
 
@@ -31,6 +31,6 @@ def test():
     GPIO.output(STBY, GPIO.LOW)
     pwm_a.stop()
     GPIO.cleanup()
-    print("Done. Did motor A spin in DIFFERENT directions?")
+    print("Done. Did motor A change direction?")
 
 test()
